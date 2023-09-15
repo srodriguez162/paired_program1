e8112f61478162e8adce55d633ed0fbca189e532
srodriguez162 <142819604+srodriguez162@users.noreply.github.com>
2023-09-15 14:25:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# Christopher Barragan, Santiago Rodriguez
+# String Methods Practice #1
+# Print the following text in uppercase, using the specific string method:
+# "Especially in electronic communications, writing in all caps is equivalent to yelling."
+sentence = "Especially in electronic communications, writing in all caps is equivalent to yelling."
+
+print(sentence.upper())
+
+# String Methods Practice #2
+# Join the following list into a string, separating each item with a space. Use the appropriate list/string method, and display the result.
+word_list = ["Simple","is","better","than","complex."]
+word_list2 = " ".join(word_list)
+print(word_list2)
+
+# String Methods Practice #3
+# Replace in the following sentence:
+# "If the implementation is hard to explain, it might be a bad idea."
+# the following pairs of words:
+# "hard" --> "easy"
+# "bad" --> "good"
+# and display the sentence with both words modified.
+
+sentence2 = "If the implementation is hard to explain, it might be a bad idea."
+result = sentence2.replace("hard", "easy")
+result2 = result.replace("bad", "good")
+print(result2)
+#################################string properties################################
+
+# String Properties Practice #1
+# Concatenate the text "Repetition" 15 times and display the result on the screen.
+# Luckily, you know that strings are multipliable and you can do this activity in a simple and elegant way.
+word = "repetition "
+print(word * 15)
+# String Properties Practice #2
+# Check if the word "beach" is not found in the following haiku. You should print the boolean.
+# "Whitecaps on the bay:
+# A broken signboard banging
+# In the April wind."
+# — Richard Wright, collected in Haiku: This Other World, 1998
+haiku = "Whitecaps on the bay: A broken signboard banging In the April wind. - Richard Wright, collected in Haiku: This Other World, 1998" 
+print(haiku.find("beach"))
+
+# String Properties Practice #3
+# Check the Python Documentation to find the description of the len() function. Then, display on the screen the length (in number of characters) of the word electroencephalographist.
+longWord = "electroencephalographist"
+print(len(longWord))
